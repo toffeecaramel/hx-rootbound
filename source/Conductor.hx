9ebeff37547e58cb61ff98645b63e5d59cbd6936
Toffee <160152673+toffeecaramel@users.noreply.github.com>
2025-08-03 21:54:34 -0300

    basic conductor test

---
+++ source/Conductor.hx
@@ -0,0 +1,64 @@
+package;
+
+import flixel.FlxG;
+import flixel.util.FlxSignal;
+import flixel.FlxBasic;
+
+class Conductor extends FlxBasic
+{
+    public var bpm(default, set):Float = 120;
+    public var songPosition:Float = 0;
+    public var songOffset:Float = 0;
+
+    public var beatLength:Float;
+    public var crochet:Float;
+    public var stepLength:Float; 
+
+    public var onBeatHit:FlxSignal = new FlxSignal();
+    public var onStepHit:FlxSignal = new FlxSignal();
+
+    private var curBeat:Int = 0;
+    private var curStep:Int = 0;
+
+    public function new(bpm:Float = 120)
+    {
+        super();
+        this.bpm = bpm;
+    }
+
+    override public function update(elapsed:Float):Void
+    {
+        super.update(elapsed);
+
+        if(FlxG.sound.music != null && FlxG.sound.music.playing)
+        	songPosition = FlxG.sound.music.time + songOffset;
+
+		while (curStep < Math.floor(songPosition / stepLength))
+		{
+		    curStep++;
+		    onStepHit.dispatch();
+		}
+
+		while (curBeat < Math.floor(songPosition / beatLength))
+		{
+		    curBeat++;
+		    onBeatHit.dispatch();
+		}
+    }
+
+    public function reset(songStartTime:Float = 0):Void
+    {
+        songPosition = songStartTime;
+        curStep = 0;
+        curBeat = 0;
+    }
+
+    public function set_bpm(newBPM:Float):Float
+    {
+        bpm = newBPM;
+        crochet = beatLength = (60 / bpm) * 1000;
+        stepLength = crochet / 4;
+
+        return bpm;
+    }
+}
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,6 +7,15 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		super.create();
+
+		flixel.FlxG.sound.playMusic(AssetPaths.Inst_erect__ogg);
+		var conductor = new Conductor(135);
+		add(conductor);
+
+		conductor.onBeatHit.add(() -> {
+			flixel.FlxG.sound.play(AssetPaths.metronome__ogg);
+			trace('a');
+		});
 	}
 
 	override public function update(elapsed:Float)
